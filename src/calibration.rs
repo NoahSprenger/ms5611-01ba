7d0e51c9a8cfa652a0fb3b1681557765f8410504
NoahSprenger <noah.r.sprenger@gmail.com>
2024-07-14 22:19:27 -0400

    Implement commands, delays, and error handling.

--- src/calibration.rs
+++ src/calibration.rs
@@ -1,7 +1,5 @@
-
-
-
 /// Oversampling Ratio
+#[derive(Clone)]
 pub enum OversamplingRatio {
     OSR256,
     OSR512,
@@ -20,13 +18,37 @@ impl OversamplingRatio {
             OversamplingRatio::OSR4096 => 0x08,
         }
     }
+    pub fn delay(&self) -> u32 {
+        // 0.5 / 1.1 / 2.1 / 4.1 / 8.22 ms return in ns
+        match *self {
+            OversamplingRatio::OSR256 => 500_000,
+            OversamplingRatio::OSR512 => 1_100_000,
+            OversamplingRatio::OSR1024 => 2_100_000,
+            OversamplingRatio::OSR2048 => 4_100_000,
+            OversamplingRatio::OSR4096 => 8_220_000,
+        }
+    }
 }
 
+#[derive(Clone, Copy)]
 pub struct Calibration {
     pub sens: u16,
     pub off: u16,
-    tcs: u16, // temperature coefficient of sensitivity
-    tco: u16, // temperature coefficient of offset
-    t_ref: u16,
-    temp_sens: u16,
+    pub tcs: u16, // temperature coefficient of sensitivity
+    pub tco: u16, // temperature coefficient of offset
+    pub t_ref: u16,
+    pub temp_sens: u16,
+}
+
+impl Calibration {
+    pub fn new(buf: &[u16; 8]) -> Calibration {
+        Calibration {
+            sens: buf[1],
+            off: buf[2],
+            tcs: buf[3],
+            tco: buf[4],
+            t_ref: buf[5],
+            temp_sens: buf[6],
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,36 @@
 #![no_std]
-use embedded_hal::spi::SpiDevice;
+use embedded_hal::spi::{Operation, SpiDevice};
 mod calibration;
 mod command;
 mod error;
 use calibration::{Calibration, OversamplingRatio};
 use command::Command;
+use embedded_hal::delay::DelayNs;
 use error::DeviceError;
 
 pub struct MS5611_01BA<SPI> {
     spi: SPI,
-    calibration: Option<Calibration>, // a user can choose to calibrate the device
+    calibration: Result<Calibration, DeviceError<SPI>>, // a user can choose to calibrate the device
     oversampling_ratio: OversamplingRatio,
 }
 
 impl<SPI> MS5611_01BA<SPI>
 where
-    SPI: SpiDevice,
+    SPI: SpiDevice<Error = SPI>,
 {
     /// Create a new instance of the MS5611_01BA03
     /// Clock speed must not exceed 20MHz
     /// Accept mode 0 or 3.
     /// Default to OSR1024
-    pub fn new(spi: SPI) -> Self {
+    pub fn new(mut spi: SPI, oversampling_ratio: OversamplingRatio) -> Self {
         // Should we calibrate in the constructor? No because this is beyond the scope of the constructor
+
+        let calibration = Self::calibrate(&mut spi);
 
         Self {
             spi,
-            calibration: None,
-            oversampling_ratio: OversamplingRatio::OSR1024,
+            calibration,
+            oversampling_ratio,
         }
     }
     /// Every module is individually factory calibrated at two temperatures and two pressures. As a result, 6 coefficients
@@ -36,42 +39,117 @@ where
     /// and used in the program converting D1 and D2 into compensated pressure and temperature values.
     /// This could just be a function of the device struct
     /// Only needs to be called once.
-    pub fn calibrate(&mut self) -> Result<(), DeviceError<SPI::Error>> {
+    fn calibrate(spi: &mut SPI) -> Result<Calibration, DeviceError<SPI::Error>> {
         // Read PROM
         // Calculate calibration values
+        //  Address 0 contains factory data and the setup, addresses 1-6 calibration coefficients and address 7 contains
+        // the serial code and CRC.
+        let mut buf = [0u16; 8]; // 16 bits
+        let mut temp_buf = [0u8; 2];
 
-        Ok(())
+        for i in 0..8 {
+            spi.transaction(&mut [
+                Operation::Write(&[Command::ReadPROM(i / 4, (i / 2) % 2, i % 2).value()]),
+                Operation::Read(&mut temp_buf),
+            ])
+            .map_err(DeviceError::Spi)?;
+            buf[i as usize] = u16::from_be_bytes(temp_buf);
+        }
+
+        // before setting calibration values, check CRC.
+        // 4 bit CRC
+        if !crc4(&mut buf) {
+            return Err(DeviceError::InvalidCRC);
+        }
+
+        let calibration = Calibration::new(&buf);
+
+        Ok(calibration)
     }
 
-    fn set_oversampling_ratio(&mut self, ratio: OversamplingRatio) {
+    pub fn set_oversampling_ratio(&mut self, ratio: OversamplingRatio) {
         self.oversampling_ratio = ratio;
     }
 
-    fn read_digital_temp(&mut self) -> Result<i32, DeviceError<SPI::Error>> {
-        Ok(0)
+    pub fn reset(&mut self) -> Result<(), DeviceError<SPI::Error>> {
+        self.spi
+            .transaction(&mut [Operation::Write(&[Command::Reset.value()])])
+            .map_err(DeviceError::Spi)?;
+        Ok(())
     }
 
-    fn read_digital_pressure(&mut self) -> Result<i32, DeviceError<SPI::Error>> {
-        Ok(0)
+    fn read_digital_temp<Delay: DelayNs>(
+        &mut self,
+        delay: &mut Delay,
+    ) -> Result<u32, DeviceError<SPI::Error>> {
+        // send d2 conversion command
+        // wait for conversion
+        // read digital temperature
+        let mut temp_buf = [0u8; 4];
+        self.spi
+            .transaction(&mut [Operation::Write(&[Command::D2Conversion(
+                self.oversampling_ratio.clone(),
+            )
+            .value()])])
+            .map_err(DeviceError::Spi)?;
+
+        delay.delay_ns(self.oversampling_ratio.delay());
+
+        self.spi
+            .transaction(&mut [
+                Operation::Write(&[Command::ReadADC.value()]),
+                Operation::Read(&mut temp_buf[1..]), // skip first byte. ADC return is 24 bits.
+            ])
+            .map_err(DeviceError::Spi)?;
+
+        Ok(u32::from_be_bytes(temp_buf))
     }
 
-    pub fn get_temperature_uncompensated(&mut self) -> Result<f32, DeviceError<SPI::Error>> {
+    fn read_digital_pressure<Delay: DelayNs>(
+        &mut self,
+        delay: &mut Delay,
+    ) -> Result<u32, DeviceError<SPI::Error>> {
+        let mut temp_buf = [0u8; 4];
+        self.spi
+            .transaction(&mut [Operation::Write(&[Command::D1Conversion(
+                self.oversampling_ratio.clone(),
+            )
+            .value()])])
+            .map_err(DeviceError::Spi)?;
+
+        delay.delay_ns(self.oversampling_ratio.delay());
+
+        self.spi
+            .transaction(&mut [
+                Operation::Write(&[Command::ReadADC.value()]),
+                Operation::Read(&mut temp_buf[1..]), // skip first byte. ADC return is 24 bits.
+            ])
+            .map_err(DeviceError::Spi)?;
+
+        Ok(u32::from_be_bytes(temp_buf))
+    }
+
+    fn get_temperature_uncompensated<Delay: DelayNs>(
+        &mut self,
+        delay: &mut Delay,
+    ) -> Result<(i32, i32), DeviceError<SPI>> {
         // Read digital temperature
         // Calculate temperature
-        Ok(0.0)
-    }
+        let digital_temp = self.read_digital_temp(delay)?;
 
-    pub fn get_pressure_uncompensated(&mut self) -> Result<f32, DeviceError<SPI::Error>> {
-        // Read digital pressure
-        // Calculate pressure
-        Ok(0.0)
+        if let Ok(ref calibration) = self.calibration {
+            let d_t: i32 = digital_temp as i32 - calibration.t_ref as i32;
+            let temp = 2000 + d_t * calibration.temp_sens as i32 / 2_i32.pow(23);
+            return Ok((temp, d_t));
+        }
+        Err(DeviceError::Uncalibrated)
     }
 
     /// Second Order Temperature Compensation
     fn temp_compensate(&self, temp: i32, d_t: i32) -> Result<(i32, i64, i64), DeviceError<SPI>> {
         // We assume that we are in high temperature unless sensed otherwise.
         // I don't really like how this is just mutables galore.
-        if let Some(ref calibration) = self.calibration {
+        if let Ok(ref calibration) = self.calibration {
             let mut t2: i32 = 0;
             let mut off2: i64 = 0;
             let mut sens2: i64 = 0;
@@ -96,25 +174,47 @@ where
         }
     }
 
-    pub fn get_temperature(&mut self) -> Result<f32, DeviceError<SPI::Error>> {
-        // Get uncompensated temperature
-        // Get digital temperature
-        // Get uncompensated pressure
-        // Get digital pressure
-        // Get temperature compensated pressure
-        // Get pressure compensated temperature
-        Ok(0.0)
+    /// Returns (pressure mbar, temperature celcius)
+    pub fn get_data<Delay: DelayNs>(
+        &mut self,
+        delay: &mut Delay,
+    ) -> Result<(i32, i32), DeviceError<SPI::Error>> {
+        let (temp, d_t) = self.get_temperature_uncompensated(delay)?;
+        let (temp, mut off, mut sens) = self.temp_compensate(temp, d_t)?;
+        let d1 = self.read_digital_pressure(delay)?;
+        if let Ok(ref calibration) = self.calibration {
+            off += calibration.tco as i64 * d_t as i64;
+            sens += calibration.tcs as i64 * d_t as i64;
+            let p = d1 as i64 * sens - off;
+            return Ok((p as i32, temp));
+        }
+        Err(DeviceError::Uncalibrated)
     }
+}
 
-    pub fn get_pressure(&mut self) -> Result<f32, DeviceError<SPI::Error>> {
-        // Get uncompensated temperature
-        // Get digital temperature
-        // Get uncompensated pressure
-        // Get digital pressure
-        // Get temperature compensated pressure
-        // Get pressure compensated temperature
-        Ok(0.0)
+/// CRC4 is calculated from the 16 bits of the PROM. The CRC is calculated by the polynomial x^4 + x^3 + x^2 + 1.
+/// This should live elsewhere since it's not really a method of the device.
+fn crc4(prom: &mut [u16; 8]) -> bool {
+    let mut n_rem = 0_u16;
+    let crc_read = prom[7] & 0xF; // last 4 bits
+    prom[7] = prom[7] & 0xFF00; // clear last 4 bits
+    for i in 0..16 {
+        if i % 2 == 1 {
+            n_rem ^= prom[i >> 1] & 0x00FF;
+        } else {
+            n_rem ^= prom[i >> 1] >> 8;
+        }
+        for _ in 0..8 {
+            if n_rem & 0x8000 != 0 {
+                n_rem = (n_rem << 1) ^ 0x3000;
+            } else {
+                n_rem = n_rem << 1;
+            }
+        }
     }
+    n_rem = n_rem >> 12;
+    n_rem ^= 0x00;
+    crc_read == n_rem
 }
 
 #[cfg(test)]
